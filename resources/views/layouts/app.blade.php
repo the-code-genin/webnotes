d26878c729bbe0d346a860dccb6127898704ccd9
Iyiola-am <adekunle3317@gmail.com>
2019-12-08 11:15:11 +0100

    App layout refactored

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@section('title')Web Notes @show</title>
+    <meta name="viewport" content="width=device-width;intial-scale=1.0" />
+    <link rel="stylesheet" href="{{ url('bootstrap-4.3.1-dist/css/bootstrap.min.css') }}" />
+</head>
+<body>
+    
+    <div class="container my-5">
+        <div class="row">
+
+            {{-- Page header --}}
+            <header class="col-sm-12">
+                <h1 class="text-center"><a href="{{ route('index') }}" class="text text-warning">Web Notes</a></h1>
+            </header>
+
+            {{-- Page content --}}
+            <section class="col-sm-8 offset-sm-2 my-5">
+
+                {{-- Main page content --}}
+                @yield('content')
+
+            </section>
+
+            {{-- Page footer --}}
+            <footer class="col-sm-12 box no-shadow text-center">
+                <p class="text-small">&copy; Iyiola_am, 2019</p>
+            </footer>
+
+        </div>
+    </div>
+
+    <script src="{{ url('bootstrap-4.3.1-dist/js/bootstrap.min.js') }}"></script>
+</body>
+</html>
