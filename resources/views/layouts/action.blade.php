87a2a346ef95e1b78d8a688acae852b81538e891
Iyiola-am <adekunle3317@gmail.com>
2019-12-08 11:34:39 +0100

    Note actions subview added

---
+++ resources/views/layouts/action.blade.php
@@ -0,0 +1,2 @@
+<a href="{{ route('edit', $note->id) }}" class="text text-inline-block text-success">Edit</a>
+<a href="{{ route('destroy', $note->id) }}" class="text text-inline-block text-danger">Delete</a>
