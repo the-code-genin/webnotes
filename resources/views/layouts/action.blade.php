a8d1fa94181473668b03dedb8173ba2db72502d0
Iyiola-am <adekunle3317@gmail.com>
2019-12-11 01:49:18 +0100

    Action sub view implemented

--- resources/views/layouts/action.blade.php
+++ resources/views/layouts/action.blade.php
@@ -1,2 +1,3 @@
-<a href="{{ route('notes.edit', $note->id) }}" class="text text-inline-block text-success">Edit</a>
-<a href="{{ route('notes.destroy', $note->id) }}" class="text text-inline-block text-danger">Delete</a>
+<a href="{{ route('notes.edit', $note->id) }}" class="text text-inline-block text-success">Edit</a> &nbsp;
+<form action="{{route('notes.destroy', $note->id) }}" class="ajax-form inline-lock d-none" method="POST">{{ csrf_field() }}{{ method_field('DELETE') }}</form> &nbsp;
+<a href="javascript:(0)" class="text text-inline-block text-danger">Delete</a> &nbsp;
