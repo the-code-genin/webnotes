af9148e261cd8a722ac287455b59e8639796020d
Iyiola-am <adekunle3317@gmail.com>
2019-12-12 15:12:47 +0100

    Note deletion added to action view

--- resources/views/layouts/action.blade.php
+++ resources/views/layouts/action.blade.php
@@ -1,3 +1,3 @@
 <a href="{{ route('notes.edit', $note->id) }}" class="text text-inline-block text-success">Edit</a> &nbsp;
-<form action="{{route('notes.destroy', $note->id) }}" class="ajax-form inline-lock d-none" method="POST">{{ csrf_field() }}{{ method_field('DELETE') }}</form> &nbsp;
-<a href="javascript:(0)" class="text text-inline-block text-danger">Delete</a> &nbsp;
+<form id="note_{{$note->id}}" action="{{route('notes.destroy', $note->id) }}" class="ajax-form d-none" method="POST">{{ csrf_field() }}{{ method_field('DELETE') }}</form> &nbsp;
+<a href="javascript:(0)" class="text text-inline-block text-danger delete-btn" data-id="note_{{ $note->id }}">Delete</a> &nbsp;
